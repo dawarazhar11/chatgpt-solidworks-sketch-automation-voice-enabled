ee980963493520a18d9f0608c54dc185f1cc0eef
dawarazhar11 <dawar.azhar@yahoo.com>
2023-03-17 09:56:02 +0500

    SolidWorks Sketch Automation with ChatGPT integration
    This commit sets up the project structure and includes necessary files for automating sketch creation in SOLIDWORKS using ChatGPT API and speech recognition. The project integrates ChatGPT, SOLIDWORKS API, and speech recognition to provide an interactive and efficient approach to creating sketches in SOLIDWORKS based on user's verbal commands. The README contains installation and usage instructions.

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# chatgpt-solidworks-sketch-automation-voice-enabled
- 
+# SolidWorks Sketch Automation
+
+This project demonstrates how to use the ChatGPT API and SOLIDWORKS API with speech recognition to automate sketch creation in SOLIDWORKS based on user's verbal commands. The project integrates ChatGPT, SOLIDWORKS API, and speech recognition to provide an interactive and efficient approach to creating sketches in SOLIDWORKS.
+
+## Description
+
+The Python script `solidworks_sketch.py` listens to verbal commands, interprets them using the ChatGPT API, and generates corresponding sketches in SOLIDWORKS through the SOLIDWORKS API. Users can create various sketch types and add dimensions based on their spoken input.
+
+## Installation
+
+1. Clone this repository or download the source code.
+
+2. Install the required Python packages:
+
+```bash
+pip install -r requirements.txt
+
+3. Copy the solidworks.py file from the pySW repository into your project folder.
+
+4. Set up your ChatGPT API key in the solidworks_sketch.py script. Replace 'your_api_key' with your actual ChatGPT API key.
+
+## Usage
+
+1. Open a terminal or command prompt, navigate to the project folder, and run the following command:
+
+python solidworks_sketch.py
+
+2. The script will listen for your verbal commands to create the desired sketch in SOLIDWORKS.
+
+3. After creating the sketch, the script will prompt you to provide dimensions verbally.
+
+4. The script will add dimensions to the sketch based on your input.
+
+## Contributing
+
+Feel free to submit issues, feature requests, and pull requests to improve the project.
+
+## License
+
+MIT License
---
+++ solidworks_sketch.py
@@ -0,0 +1,81 @@
+import openai
+from solidworks import SOLIDWORKS
+import speech_recognition as sr
+
+# Replace 'your_api_key' with your actual ChatGPT API key
+openai.api_key = "your_api_key"
+
+sw_app = SOLIDWORKS()
+sw_app.connect()
+
+def get_chatgpt_response(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    return response.choices[0].text.strip()
+
+def recognize_speech():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening for your command...")
+        audio = recognizer.listen(source)
+
+    try:
+        text = recognizer.recognize_google(audio)
+        print(f"You said: {text}")
+        return text
+    except sr.UnknownValueError:
+        print("Sorry, could not understand your command.")
+        return None
+    except sr.RequestError as e:
+        print(f"Error: {e}")
+        return None
+
+def create_sketch(sketch_type, *args):
+    if sketch_type.lower() == "rectangle":
+        instruction = get_chatgpt_response("How do I create a rectangle sketch in SOLIDWORKS using Python API?")
+        print("Instruction:", instruction)
+        sw_app.create_sketch()
+        sw_app.draw_rectangle(*args)
+        
+    elif sketch_type.lower() == "circle":
+        instruction = get_chatgpt_response("How do I create a circle sketch in SOLIDWORKS using Python API?")
+        print("Instruction:", instruction)
+        sw_app.create_sketch()
+        sw_app.draw_circle(*args)
+        
+    # Add other sketch types here
+    else:
+        print("Invalid sketch type")
+
+def add_dimensions(dimensions):
+    # Add dimensions to the sketch using SOLIDWORKS API based on the provided dimensions
+    pass
+
+if __name__ == "__main__":
+    while True:
+        user_command = recognize_speech()
+        if user_command:
+            sketch_type = get_chatgpt_response(f"Create {user_command} sketch in SOLIDWORKS")
+            print(f"Creating a {sketch_type} sketch...")
+
+            # You can add more conditions here to handle additional sketch types
+            if sketch_type.lower() == "rectangle":
+                create_sketch("rectangle", 0, 0, 0.1, 0.1)
+            elif sketch_type.lower() == "circle":
+                create_sketch("circle", 0, 0, 0.05)
+            else:
+                print("Invalid sketch type")
+
+            print("Sketch created.")
+            print("Please provide the dimensions verbally.")
+            dimensions_command = recognize_speech()
+            if dimensions_command:
+                dimensions = get_chatgpt_response(f"Add {dimensions_command
